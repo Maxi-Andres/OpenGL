8188d5159f8315d4f77753861d49edc5baf60eb0
Maxi <machugab@gmail.com>
2026-01-18 19:47:02 -0300

    Rendering multiple objects and fixing multiplication within the shader

---
+++ src/1-getting-started/5-transformations/5.3-rendering-multiple-objects/IndexBuffer.h
@@ -0,0 +1,19 @@
+#pragma once
+// #include <GLFW/glfw3.h>
+
+class IndexBuffer
+{
+private:
+  unsigned int m_RendererID;
+  unsigned int m_Count;
+
+public:
+  IndexBuffer(const unsigned int *data, unsigned int count); // podes cambiar el tipo de dato de data para mejorar performance
+  ~IndexBuffer();
+  // Cuando el objeto de la clase se destruye, debe llamar a glDeleteBuffers para liberar la memoria en la tarjeta de video y evitar fugas de memoria (memory leaks).
+
+  void Bind() const;
+  void Unbind() const;
+
+  inline unsigned int GetCount() const { return m_Count; }
+};
