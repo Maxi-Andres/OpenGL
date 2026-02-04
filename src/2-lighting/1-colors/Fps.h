1c47fc60d6968bc1f9f4aef0b65dfc6a58bd78cd
Maxi <machugab@gmail.com>
2026-02-03 22:12:16 -0300

    name change?

--- src/2-lighting/1-colors/Fps.h
+++
@@ -1,7 +0,0 @@
-#include "glad/glad.h" // with this the ouput doesn't say that it has linking problems
-#include <GLFW/glfw3.h>
-#include <iostream>
-
-void updateTitle(GLFWwindow *window, const std::string &title);
-
-float getFPS();
